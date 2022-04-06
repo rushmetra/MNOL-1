c734046255d4b173508542d6c7ea8bbeb96ba8b2
David Alexandre Ferreira Duarte <62104686+DvdDuarte@users.noreply.github.com>
2022-04-06 16:01:00 +0100

    Delete scripts directory

--- scripts/integral.m
+++
@@ -1,2 +0,0 @@
-% Determinar o calor do integral dfinido entre m e n com valor de erro
-solucao = integral(@(x)funcao),m,n,'AbsTol',erro)
--- scripts/trapezio.m
+++
@@ -1,7 +0,0 @@
-% valores do x_i separados por espaço
-x = [ ];
-
-% valores do f(x_i) separados por espaço
-y = [ ];
-
-solucao = trapz(x,y)
