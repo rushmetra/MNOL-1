e1997e1c67cd394ee8f0aeea7d83db14e6f9ecde
David Alexandre Ferreira Duarte <62104686+DvdDuarte@users.noreply.github.com>
2022-04-06 15:59:31 +0100

    Add files via upload

---
+++ scripts/matriz.m
@@ -0,0 +1,11 @@
+% colocar os valores da matriz entre os ;, neste momento é uma matriz x por 3
+% os valores entre ; são separados por espaço 
+% os valores decimais são separados por .
+matriz = [ ; ;];
+
+% descobrir o determinante da matriz
+determinante = det(matriz);
+
+% descobrir a inversa da matriz
+inversa = inv(matriz);
+
